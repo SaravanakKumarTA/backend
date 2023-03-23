902b6672eec12eea86a529e923145f2b1c26fdbb
SaravanakKumarTA <saravanakkumarta@gmail.com>
2023-03-24 01:53:06 +0530

    kumar

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose
 
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", req.headers.origin);
