c646cd82ce94a8629463e69c8027f675824860c1
SaravanakKumarTA <saravanakkumarta@gmail.com>
2023-03-24 01:52:29 +0530

    vro

--- server.js
+++ server.js
@@ -20,7 +20,16 @@ mongoose
 app.use(express.json());
 
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  next();
+});
 app.use(
   session({
     resave: false,
